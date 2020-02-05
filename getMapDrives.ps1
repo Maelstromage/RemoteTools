a709dd7624b863c847e62f29e648b95ea8c56411
Maelstromage <Reverse91@yahoo.com>
2020-02-05 14:04:10 -0500

    Create getMapDrives.ps1

---
+++ getMapDrives.ps1
@@ -0,0 +1,18 @@
+$HKEY = 2147483651
+$computer = "usblnd0150"
+
+
+$explorer = Get-WmiObject -ComputerName $ComputerName -Class win32_process | where {$_.name -eq "explorer.exe"}
+$sid = ($explorer.GetOwnerSid()).sid
+$user  = $explorer.GetOwner()
+
+$strKeyPath = "$sid\network"
+
+$objReg = [WMIClass]"\\$computer\root\default:StdRegProv"
+
+$arrSubKeys = $objReg.EnumKey($HKEY, $strKeyPath)
+foreach ($subKey in ($arrSubKeys.sNames))
+{
+    write-output "$subKey `t $($objReg.GetExpandedStringValue($HKEY, "$strKeyPath\$subkey", "RemotePath").svalue)"
+
+}
